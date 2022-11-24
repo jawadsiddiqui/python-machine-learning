3b8a080fb2151c38bc6c2a77b48ca81fd03f1370
Jawad Siddiqui <jsiddiqui@wsgc.com>
2022-11-24 13:05:12 +0500

    pandas model train

---
+++ model1.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+dftrain = pd.read_csv("https://stats.govt.nz/assets/Uploads/Annual-enterprise-survey/Annual-enterprise-survey-2021-financial-year-provisional/Download-data/annual-enterprise-survey-2021-financial-year-provisional-csv.csv")
+
+print(dftrain.head())
