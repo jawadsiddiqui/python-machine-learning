a6e9d6b49f8f5dc08469755f8a145846cc441e21
Jawad Siddiqui <jsiddiqui@wsgc.com>
2022-11-25 10:29:34 +0500

    python pandas matplotlib

--- python-pandas.py
+++ python-pandas.py
@@ -1,11 +1,18 @@
+import matplotlib.pyplot
 import pandas as pd
 
 dftrain = pd.read_csv("/users/jsiddiqui/Documents/survey-2021.csv")
+
 
 print(dftrain.head())
 
 
 y_train = dftrain.pop('servived')
+y_train.head()
 print(dftrain.head())
 
 print(dftrain.loc[0])
+
+print(dftrain.describe())
+dftrain.age.hist(bins=20)
+
